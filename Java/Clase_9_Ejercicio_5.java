91eeba25162b0fb00b6ae04000b5667ddb0f95bc
Luciano <lucianocortez2003@gmial.com>
2024-06-29 23:02:54 -0300

    Clase_9_ejercicio_5

---
+++ Java/Clase_9_Ejercicio_5.java
@@ -0,0 +1,19 @@
+package Java;
+
+import java.util.Scanner;
+
+public class Clase_9_Ejercicio_5 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        float nota1, nota2, nota3, suma;
+
+        // Guardamos las tres notas
+        System.out.println("Digite las 3 calificaciones: ");
+        nota1 = Float.parseFloat(entrada.nextLine());
+        nota2 = Float.parseFloat(entrada.nextLine());
+        nota3 = Float.parseFloat(entrada.nextLine());
+
+        suma = nota1 + nota2 + nota3;
+        System.out.println("\nLa suma de las notas es: " + suma);
+    }
+}
